00bcde910653c62a965b59b39bc214976559b6ed
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-07-26 16:05:32 -0400

    Finished Exercises

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jeremy";
+let lastName;
+var age;
 
 // Exercise 2
 
+lastName = "Cabrera";
+age = 28;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age}.`);
+
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`It is ${isCaseSensitive} that ${language} was created in ${createdYear}.`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`The products ${selectedSize} size price is $${price}.`);
+console.log(`Us having it on sale for ${salePercentage}% is ${isOnSale} and in stock is ${inStock}. We have ${stock} in stock.`);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${author} is the author of ${title}. It is ${pageCount} pages long amd I am on page ${bookmark}. Even if it is ${hasRead} that I already read it.`);
